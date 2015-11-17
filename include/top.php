15161f5c93f8f3da2f6636e558a8b889633292e8
Yvonne <Yvonne@DESKTOP-N7DUH5K>
2015-11-17 13:00:45 -0700

    top include file

---
+++ include/top.php
@@ -0,0 +1,24 @@
+<?php
+
+/* top include file
+ * 
+ * created by Yvonne Lu 11-12-14
+ */
+
+if(!defined('TOP_DIR')){
+    define('TOP_DIR', '');
+}
+?>
+<!--
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>-->
+
+<link rel="stylesheet" href="<?php echo TOP_DIR.'include/jquery-ui-themes-1.11.4/themes/smoothness/jquery-ui.css';?>">
+<script src="<?php echo TOP_DIR.'include/jquery-1.11.3.min.js';?>"></script>
+<script src="<?php echo TOP_DIR.'include/jquery-ui-1.11.4/jquery-ui.min.js';?>"></script>
+<link rel="stylesheet" href="<?php echo TOP_DIR.'css/buttonStyle.css';?>" media="all">
+<link rel="stylesheet" href="<?php echo TOP_DIR.'css/shared.css';?>" media="all">
+<link rel="stylesheet" href="<?php echo TOP_DIR.'css/EDIStyle.css';?>" media="all">
+<script src="<?php echo TOP_DIR.'js/Export.js';?>"></script>
+
