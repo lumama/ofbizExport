10e37a9f4fd48ac0830575bfac5a2f910759f7af
Yvonne <Yvonne@DESKTOP-N7DUH5K>
2015-11-17 13:01:46 -0700

    Server include file

---
+++ include/server_names.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * This file defines server destination
+ * 
+ * hould be included when global RUN_STATUS is set to SERVER
+ */
+
+
+
+//dsn
+$names['dsn'] = "Driver={Microsoft Visual FoxPro Driver};SourceType=DBF;SourceDB=\\\\server\\h\\sbt\\;Exclusive=NO;collate=Machine;NULL=NO;DELETED=NO;BACKGROUNDFETCH=NO;";
