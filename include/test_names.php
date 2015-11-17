91de79943b8b9a6015aba87a428ce56858aea811
Yvonne <Yvonne@DESKTOP-N7DUH5K>
2015-11-17 13:01:03 -0700

    Include file for testing

---
+++ include/test_names.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * This file defines testing destination
+ * 
+ * should be included when global RUN_STATUS is set to TEST
+ */
+
+
+
+$names['cust_xml'] = "c:\\sbt\\ofbizexport\\export\\cust.xml"; //customer
+$names['prod_xml'] = "c:\\sbt\\ofbizexport\\export\\prod.xml"; //product
+
+
+//dsn
+$names['dsn'] = "Driver={Microsoft Visual FoxPro Driver};SourceType=DBF;SourceDB=c:\\sbt\\ofbizexport;Exclusive=NO;collate=Machine;NULL=NO;DELETED=NO;BACKGROUNDFETCH=NO;";
